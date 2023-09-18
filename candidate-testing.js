c099eaccae7bfd25db3f75326ee15a9926190d77
joshuatucker0914 <joshuatucker0914@gmail.com>
2023-09-18 06:14:00 -0500

    Test Part 1 with workflow active

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,5 +1,5 @@
 const input = require('readline-sync');
-
+//My workflow wasn't active when I pushed inititally.
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
@@ -11,9 +11,15 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = "";
-let correctAnswers = "";
-let candidateAnswers = "";
+let questions = [
+"Who was the first American woman in space? ", 
+"True or false: 5 kilometer == 5000 meters? ",
+"(5 + 3)/2 * 10 = ?",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [ "Sally Ride", "true","40","Trajectory",3 ];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,8 +29,7 @@ userName = input.question("What is your name?");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question("Who was the first woman in space? ");
-
+candidateAnswer = input.question(`${question}`);
 }
 
 function gradeQuiz(candidateAnswers) {
